640e562d2429f3bd31ffe2a2d877116991ef4cb5
impy7 <Impy@miki11>
2023-07-13 18:37:59 +0200

    Snapshot Classe Tassello

--- app/src/main/java/mp/project/mastermind/models/Tassello.kt
+++ app/src/main/java/mp/project/mastermind/models/Tassello.kt
@@ -1,4 +1,17 @@
 package mp.project.mastermind.models
 
 class Tassello {
+    // only way to get numerical value of color: Color.NONE.ordinal
+    enum class Color {NONE, WHITE, YELLOW, RED, GREEN, LBLUE, ROSE, BROWN, BLACK}
+
+    private var color = Color.NONE
+
+    fun setColor(id : Color){
+        color = id
+    }
+
+    fun getColor(): Color {
+        return color
+    }
+
 }
