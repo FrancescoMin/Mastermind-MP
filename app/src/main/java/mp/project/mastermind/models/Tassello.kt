3c94b16591d7aa97a365dd2084e2ce5765899909
impy7 <Impy@miki11>
2023-07-14 19:16:20 +0200

    Fix Tassello.Color enumeration

--- app/src/main/java/mp/project/mastermind/models/Tassello.kt
+++ app/src/main/java/mp/project/mastermind/models/Tassello.kt
@@ -2,7 +2,16 @@ package mp.project.mastermind.models
 
 class Tassello {
     // only way to get numerical value of color: Color.NONE.ordinal
-    enum class Color {NONE, WHITE, YELLOW, RED, GREEN, LBLUE, ROSE, BROWN, BLACK}
+    enum class Color {
+        NONE, WHITE, YELLOW, RED, GREEN, LBLUE, ROSE, BROWN, BLACK;
+
+        companion object{
+            private val map = Color.values().associateBy { it.ordinal }
+            infix fun from(value: Int): Color?{
+                return map[value]
+            }
+        }
+    }
 
     private var color = Color.NONE
 
