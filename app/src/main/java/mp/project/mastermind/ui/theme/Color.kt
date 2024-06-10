2b418459f5f3893c9e84f4d271da7616180936c2
jadepics <116430207+jadepics@users.noreply.github.com>
2024-06-10 15:27:44 +0200

    errori corretti

---
+++ app/src/main/java/mp/project/mastermind/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package mp.project.mastermind.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+        val Purple200 = Color(0xFFBB86FC)
+        val Purple500 = Color(0xFF6200EE)
+        val Purple700 = Color(0xFF3700B3)
+        val Teal200 = Color(0xFF03DAC5)
+        val MastermindColor = Color(0xFFE4CCFF)
+        val orange = Color(0xFFFB4207)
+        val giallo = Color(0xFFFBF207)
+        val verde = Color(0xFF07FF5C)
+        val rosso = Color(0xFFFB0707)
+        val cyan = Color(0xFF07FBDE)
+        val rosa = Color(0xFFF68FFF)
+        val blu = Color(0xFF0C24F7)
+        val black = Color.Black
+        val grey = Color.Gray
+
+
